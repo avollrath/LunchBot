cc363c5e85afd2ab9010898d920a4987117b950b
André Vollrath <46895425+avollrath@users.noreply.github.com>
2019-10-22 19:42:36 +0300

    added bot

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const SlackBot = require('slackbots');
-var fs = require('fs');
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+let menuData;
 
 request('https://www.delicatessen.fi/lounaslistat/klondyke', (error, response, html) => {
   if (!error && response.statusCode == 200) {
 
     const $ = cheerio.load(html);
 
-  const menuData = $('.newsText ul').text();
+  menuData = $('.newsText ul').text();
 
-
-
-
-
+  console.log(menuData);
 
   }})
+
+  const bot = new SlackBot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'LunchBot'
+})
+
+bot.on('start', () => {
+  const params = {
+      icon_emoji: ':robot_face:'
+  }
+
+  bot.postMessageToUser(
+      'andre.vollrath',
+      ":pizza: You look hungry lovely human. It's time to get some nutrition soon" + menuData,
+      params
+  );
+})
+
+bot.on('error', (err) => {
+  console.log(err);
+})
 
  
 
