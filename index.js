6ab10a1e3548990eaa400dd3354cdf1a99de847c
André Vollrath <46895425+avollrath@users.noreply.github.com>
2019-10-22 20:28:47 +0300

    added filter for weekdays

--- index.js
+++ index.js
@@ -3,7 +3,29 @@ const cheerio = require('cheerio');
 const SlackBot = require('slackbots');
 const dotenv = require('dotenv')
 
+
+
+let date = new Date();
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[date.getDay()];
+
+console.log(n);
+
 dotenv.config()
+
+const bot = new SlackBot({
+  token: `${process.env.BOT_TOKEN}`,
+  name: 'LunchBot'
+})
+
 
 let menuData;
 
@@ -14,30 +36,42 @@ request('https://www.delicatessen.fi/lounaslistat/klondyke', (error, response, h
 
   menuData = $('.newsText ul').text();
 
-  console.log(menuData);
+  menuData = menuData
+    .replace("Maanantai", "Monday")
+    .replace("Tiistai", "Tuesday")
+    .replace("Keskiviiko", "Wednesday")
+    .replace("Torstai", "Thursday")
+    .replace("Perjantai", "Friday");
+
+
+    let menuMonday = menuData.substring(menuData.indexOf("Monday"), menuData.indexOf("Tuesday"));
+    let menuTuesday = menuData.substring(menuData.indexOf("Tuesday"), menuData.indexOf("Wednesday"));
+    let menuWednesday = menuData.substring(menuData.indexOf("Wednesday"), menuData.indexOf("Thursday"));
+    let menuThursday = menuData.substring(menuData.indexOf("Thursday"), menuData.indexOf("Friday"));
+    let menuFriday = menuData.substring(menuData.indexOf("Friday"), menuData.length);
+
+
+
+  const params = {
+    icon_emoji: ':robot_face:'
+}
+
+  bot.postMessageToUser(
+    'andre.vollrath',
+    ":pizza: You look hungry lovely human. It's time to get some nutrition soon! \n" + menuFriday,
+    params
+);
+
 
   }})
 
-  const bot = new SlackBot({
-    token: `${process.env.BOT_TOKEN}`,
-    name: 'LunchBot'
-})
 
-bot.on('start', () => {
-  const params = {
-      icon_emoji: ':robot_face:'
-  }
-
-  bot.postMessageToUser(
-      'andre.vollrath',
-      ":pizza: You look hungry lovely human. It's time to get some nutrition soon" + menuData,
-      params
-  );
-})
 
 bot.on('error', (err) => {
   console.log(err);
 })
+
+
 
  
 
