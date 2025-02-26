1da3adc26cfcdad908cddbd3cda35f8d25b1c9c1
Andre Vollrath <andre.vollrath@talentadore.com>
2025-02-26 20:26:56 +0200

    favicon

--- index.js
+++ index.js
@@ -386,6 +386,10 @@ function createHtmlTemplate() {
   <script src="text-scramble.js" defer></script>
   <script data-goatcounter="https://pasilunch.goatcounter.com/count"
         async src="//gc.zgo.at/count.js"></script>
+<link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
+<link rel="manifest" href="assets/site.webmanifest">
 </head>
 <body>
   <!-- Hero Section with Background Video -->
